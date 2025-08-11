01301c351252bc17adbd0d646dcef339cf695b6f
alfonWaz13 <alfonWaz13@icloud.com>
2025-08-11 12:53:34 +0200

    feat: add Expense model

--- personal_finance_tracker/expenses/models.py
+++ personal_finance_tracker/expenses/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    CATEGORIES = [
+        ('food', 'Food'),
+        ('transport', 'Transport'),
+        ('bills', 'Bills'),
+        ('entertainment', 'Entertainment'),
+        ('other', 'Other'),
+    ]
+
+    category = models.CharField(max_length=50, choices=CATEGORIES)
+    date_and_time = models.DateTimeField(default=datetime.now)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.category} - {self.amount}'
