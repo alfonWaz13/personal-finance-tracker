b4c3ef8ddd8a1f9b4a47c6aba019c24963d0d120
alfonWaz13 <alfonWaz13@icloud.com>
2025-08-12 14:03:53 +0200

    feat: add delete buttons for expenses and incomes

--- incomes/urls.py
+++ incomes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('add_income', views.add_income, name='add_income'),
+    path('delete_income/<int:pk>', views.delete_income, name='delete_income'),
 ]
