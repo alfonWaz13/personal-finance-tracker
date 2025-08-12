f6f68cb1b0e1822cf8767fbd9eed2715280614b0
alfonWaz13 <alfonWaz13@icloud.com>
2025-08-12 16:02:31 +0200

    refactor: split dashboard calculations in different functions

--- personal_finance_tracker/views.py
+++ personal_finance_tracker/views.py
@@ -4,12 +4,10 @@ from expenses.models import Expense
 from incomes.models import Income
 
 def dashboard(request):
-    expenses = Expense.objects.all().order_by('-date_and_time')
-    incomes = Income.objects.all().order_by('-date_and_time')
+    expenses = get_expenses()
+    incomes = get_incomes()
 
-    total_expenses = sum(expense.amount for expense in expenses)
-    total_incomes = sum(income.amount for income in incomes)
-    total_balance = total_incomes - total_expenses
+    total_balance = get_total_balance(expenses, incomes)
 
     context = {
         'expenses': expenses,
@@ -18,3 +16,18 @@ def dashboard(request):
     }
 
     return render(request, 'dashboard.html', context)
+
+
+def get_total_balance(expenses, incomes):
+    total_expenses = sum(expense.amount for expense in expenses)
+    total_incomes = sum(income.amount for income in incomes)
+    total_balance = total_incomes - total_expenses
+    return total_balance
+
+
+def get_incomes():
+    return Income.objects.all().order_by('-date_and_time')
+
+
+def get_expenses():
+    return Expense.objects.all().order_by('-date_and_time')
