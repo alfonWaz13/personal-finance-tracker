478e1c10d059d0b8b123bb29a29ba1679a77b19f
alfonWaz13 <alfonWaz13@icloud.com>
2025-08-12 13:40:20 +0200

    feat: add total balance to dashboard

--- personal_finance_tracker/views.py
+++ personal_finance_tracker/views.py
@@ -7,9 +7,14 @@ def dashboard(request):
     expenses = Expense.objects.all().order_by('-date_and_time')
     incomes = Income.objects.all().order_by('-date_and_time')
 
+    total_expenses = sum(expense.amount for expense in expenses)
+    total_incomes = sum(income.amount for income in incomes)
+    total_balance = total_incomes - total_expenses
+
     context = {
         'expenses': expenses,
         'incomes': incomes,
+        'total_balance': total_balance,
     }
 
     return render(request, 'dashboard.html', context)
